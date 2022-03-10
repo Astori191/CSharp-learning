22c64f120b38ca1c56a47b47992a04e747faf307
Astori191 <gdmreol@gmail.com>
2022-03-10 12:13:56 +0300

    cmd args and system.environment

---
+++ SimpleCSharpApp/SimpleCSharpApp/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleCSharpApp
+{
+    internal class Program
+    {
+        static int Main(string[] args)
+        {
+            Console.WriteLine("My First C# App");
+            Console.WriteLine("Hello World");
+
+            //обработать любые входные аргументы
+            for (int i = 0; i < args.Length; i++)
+                Console.WriteLine("Arg: {0}", args[i]);
+
+            //обработать любые входные аргументы, используя foreach
+            foreach (string arg in args)
+                Console.WriteLine("Arg: {0}", arg);
+
+            //получить агрументы с использованием System.Environment
+            string[] theArgs = Environment.GetCommandLineArgs();
+            foreach (string arg in theArgs)
+                Console.WriteLine("Arg: {0}", arg);
+
+            ShowEnvironmentDetails();
+
+            Console.ReadLine();
+            return 0;
+        }
+
+        static void ShowEnvironmentDetails()
+        {
+            //вывести информацию о дисковых устройствах и другие интересные детали
+            Console.WriteLine();
+            foreach (string drive in Environment.GetLogicalDrives())
+                Console.WriteLine("Drive: {0}", drive);
+            Console.WriteLine("OS: {0}", Environment.OSVersion);
+            Console.WriteLine("Number of processors: {0}", Environment.ProcessorCount);
+            Console.WriteLine(".NET Version {0}", Environment.Version);
+        }
+    }
+}

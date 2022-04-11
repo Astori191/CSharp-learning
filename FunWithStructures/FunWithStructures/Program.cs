35fda7ac42095ffd5c356b4a4d886f3db7df1c48
Astori191 <gdmreol@gmail.com>
2022-04-11 21:55:43 +0300

    Structures

---
+++ FunWithStructures/FunWithStructures/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FunWithStructures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("***** A First Look at Structures *****\n");
+
+            // Создать начальную переменную типа Point.
+            Point myPoint;
+            myPoint.X = 349;
+            myPoint.Y = 76;
+            myPoint.Display();
+
+            // Скорректировать значения X и Y.
+            myPoint.Increment();
+            myPoint.Display();
+
+            /*
+                Ошибка! Полю Y не присвоено значение.
+                Point p1;
+                p1.X = 10;
+                p1.Display(); 
+            */
+
+            Point p2;
+            p2.X = 10;
+            p2.Y = 10;
+            p2.Display();
+
+            // Установить для всех полей стандартные значения
+            // используя стандартный конструктор.
+            Point p1 = new Point();
+            p1.Display();
+
+            // Вызвать специальный конструктор.
+            Point p3 = new Point(50, 60);
+            // Выводит X=50,Y=60
+            p3.Display();
+        }
+    }
+
+    struct Point
+    {
+        // Поля структуры
+        public int X;
+        public int Y;
+
+        // Специальный конструктор
+        public Point(int XPos, int YPos)
+        {
+            X = XPos;
+            Y = YPos;
+        }
+
+        // Добавить 1 к позиции (X, Y)
+        public void Increment()
+        {
+            X++;
+            Y++;
+        }
+
+        // Вычесть 1 из позиции (X, Y)
+        public void Decrement()
+        {
+            X--;
+            Y--;
+        }
+
+        // Отобразить текущую позицию,
+        public void Display()
+        {
+            Console.WriteLine($"X = {X}, Y = {Y}");
+        }
+
+    }
+}
